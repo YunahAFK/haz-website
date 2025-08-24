f9b2c6abdf0bd4db210c70f303e0b1ec852594ad
YunahAFK <startingfromzero.04302025@gmail.com>
2025-08-24 21:11:50 +0800

    add: deployment config

--- src/components/hazard/ContentTab.tsx
+++ src/components/hazard/ContentTab.tsx
@@ -449,7 +449,11 @@ const ContentTab: React.FC = () => {
               onChange={(e) => handleInputChange('content', e.target.value)}
               rows={15}
               className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent resize-none font-mono text-sm"
-              placeholder="Enter your lecture content here..."
+              placeholder="tools to be added for creating lecture
+              - font heading tool
+              - add image tool
+              - visual guide lines for clear lecture separations
+              - ..."
             />
           </div>
 
